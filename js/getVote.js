1c5ec1c16a9d16a9df0bb8d0285855ef64035f13
jeslundg <jesper.lundgren2@gmail.com>
2020-04-22 17:10:10 +0200

    Added JavaScript to save votes in an array

---
+++ js/getVote.js
@@ -0,0 +1,26 @@
+const queueVoter = document.querySelector('.queueVoter');
+const keys = queueVoter.querySelector('.voteButton');
+let voteArray = [];
+const maxArrayLength  = 5;
+
+
+keys.addEventListener('click', e => {
+
+    // Används matches??
+    if (e.target.matches('button')) {
+        const key = e.target;
+        const keyContent = key.textContent;
+
+        if(voteArray.length < maxArrayLength) {
+            let vote = {queueLength: parseInt(keyContent) , time:Date.now()};
+            voteArray.push(vote);
+        }
+        else {
+            voteArray.shift();
+            let vote = {queueLength: parseInt(keyContent) , time:Date.now()};
+            voteArray.push(vote)
+        }
+        console.log(voteArray)
+
+    }
+});
