c64aa9e5b3409309280fa467d236c4d4f68c779f
Antonia <welzel@student.chalmers.se>
2020-04-23 14:19:03 +0200

    added queue indicator and function for changing it

---
+++ js/changeQueueColour.js
@@ -0,0 +1,20 @@
+
+    function changeQueueColour(x) {
+
+        if((x == 1)) {
+
+        document.getElementById("circle-1").style.backgroundColor = "red";
+
+        } else if((x == 2)) {
+
+        document.getElementById("circle-2").style.backgroundColor = "yellow";
+
+        } else if((x == 3)) {
+
+        document.getElementById("circle-3").style.backgroundColor = "green";
+
+        }
+
+    }
+
+    changeQueueColour(2);
