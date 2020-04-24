19e5186c98f8e74531b278fffb690dc91569cfa9
jeslundg <jesper.lundgren2@gmail.com>
2020-04-24 12:02:53 +0200

    Other lights now black out when switching color.

--- js/changeQueueColour.js
+++ js/changeQueueColour.js
@@ -4,18 +4,22 @@
         if((x == 3)) {
 
         document.getElementById("circle-1").style.backgroundColor = "red";
+        document.getElementById("circle-2").style.backgroundColor = "black";
+        document.getElementById("circle-3").style.backgroundColor = "black";
 
         } else if((x == 2)) {
 
         document.getElementById("circle-2").style.backgroundColor = "yellow";
-
+        document.getElementById("circle-1").style.backgroundColor = "black";
+        document.getElementById("circle-3").style.backgroundColor = "black";
         } else if((x == 1)) {
 
         document.getElementById("circle-3").style.backgroundColor = "green";
-
+        document.getElementById("circle-2").style.backgroundColor = "black";
+            document.getElementById("circle-1").style.backgroundColor = "black";
         }
 
     }
 
-    changeQueueColour(queueValue);
+   // changeQueueColour(queueValue);
     //changeQueueColour(2);
