f86dbc9f70f5ab123d08c0cef225ba42cb54e96c
Johan <johan.laberg@outlook.com>
2020-04-26 14:04:44 +0200

    Added a server that runs on localhost:3000. To run it install NodeJS or read the README.md file on github

---
+++ server.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const server = express()
+const port = 3000
+
+server.listen(process.env.PORT || port)
+
+// Responds to the get request with the main website.
+server.get('/', function (req, res) {
+    res.sendFile('index.html', {root: __dirname});
+});
+
+// Responds with the file in the image folder.
+server.get('/images/:theImage', function (req, res) {
+    res.sendFile('./images/' + req.params.theImage,{root: __dirname});
+});
+
+// Responds with the file in the views folder.
+server.get('/views/profile.html', function (req, res) {
+    res.sendFile('./views/profile.html',{root: __dirname});
+});
+
+// Responds with the file in the js folder.
+server.get('/js/:theScript', function (req, res) {
+    res.sendFile('./js/' + req.params.theScript, {root: __dirname});
+});
+
+// Responds with the file in the css folder.
+server.get('/css/:theStylesheet', function (req, res) {
+    res.sendFile('./css/' + req.params.theStylesheet, {root: __dirname});
+});
+
+// Responds with the file in the 'fonts/raleway' folder.
+server.get('/fonts/raleway/:theFont', function (req, res) {
+    res.sendFile('./fonts/raleway/' + req.params.theFont, {root: __dirname});
+});
+
+// Responds with the file in the 'fonts/lato' folder.
+server.get('/fonts/lato/:theFont', function (req, res) {
+    res.sendFile('./fonts/lato/' + req.params.theFont, {root: __dirname});
+});
