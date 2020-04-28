5f5186a14001cfba8dfb600bc01c0db227ff23b5
Antonia <welzel@student.chalmers.se>
2020-04-28 21:48:27 +0200

    Update getVote.js

--- js/getVote.js
+++ js/getVote.js
@@ -22,6 +22,6 @@ keys.addEventListener('click', e => {
 
 
         console.log(voteArray)
-        changeQueueColour(document.getElementsByClassName("col-sm-6 col-lg-4 col-xl-3 grid-element")[0].id, calculateQueue(voteArray));
+        changeQueueColour(document.getElementsByClassName("col-sm-6 col-lg-4 col-xl-3 grid-element").id, calculateQueue(voteArray));
     }
 });
