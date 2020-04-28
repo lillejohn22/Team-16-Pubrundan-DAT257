49b85e2eae63395a45e8d20ffea93cb2fdbb1827
Antonia <welzel@student.chalmers.se>
2020-04-28 21:32:53 +0200

    change backgroundcolour for all pubs

--- js/changeQueueColour.js
+++ js/changeQueueColour.js
@@ -2,22 +2,28 @@
 //Changes the class of the object with given id.
 function changeQueueColour(id, color) {
 
+
      if(color == 1) {
 
+            document.getElementById(id).classList.remove('pub-closed');
             document.getElementById(id).classList.remove('long-queue');
             document.getElementById(id).classList.remove('medium-queue');
             document.getElementById(id).classList.add('short-queue');
 
      } else if(color == 2) {
 
+            document.getElementById(id).classList.remove('pub-closed');
             document.getElementById(id).classList.remove('long-queue');
             document.getElementById(id).classList.add('medium-queue');
             document.getElementById(id).classList.remove('short-queue');
 
      } else if(color == 3) {
 
+            document.getElementById(id).classList.remove('pub-closed');
             document.getElementById(id).classList.add('long-queue');
             document.getElementById(id).classList.remove('medium-queue');
             document.getElementById(id).classList.remove('short-queue');
      }
+
+
 }
