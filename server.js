ef19f69dbe7332b3abd708274beda74453e39ccb
Johan <johan.laberg@outlook.com>
2020-04-29 10:09:08 +0200

    server is being updated wiht votings pls

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const path = require('path')
+const bodyParser = require('body-praser')
+
 const server = express()
 const port = 3000
 
@@ -15,4 +17,6 @@ server.use("/images", express.static(__dirname + '/images'))
 server.use("/css", express.static(__dirname + '/css'))
 server.use("/fonts", express.static(__dirname + '/fonts'))
 
-
+server.get('/voted', function (req, res) {
+    console.log('you just voted! This is your data: ' + req.params)
+});
