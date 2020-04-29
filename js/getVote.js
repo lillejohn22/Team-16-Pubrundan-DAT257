5db89db3bc151acfd3c2aaacf2e7de740956a007
Johan <johan.laberg@outlook.com>
2020-04-29 23:34:54 +0200

    Server now gets vote, processes vote and returns vote. Website also changes the color. @TODO: Fix the calculation of queue to set correct color value. Find a way to get the html items pubID so that the right object can be changed (currently this is hardcoded)

--- js/getVote.js
+++ js/getVote.js
@@ -1,43 +1,50 @@
 const queueVoter = document.querySelector('.queueVoter');
 const keys = queueVoter.querySelector('.voteButton');
 
-const maxArrayLength = 5;
-
-var v = 0;
-var voteArray = [];
-const url = '/voted';
-
 //Register key event and saves result on an array.
 keys.addEventListener('click', e => {
 
     if (e.target.matches('button')) {
         const key = e.target;
-        const keyContent = key.value;
-
-        if (voteArray.length < maxArrayLength) {
-            let vote = parseInt(keyContent);
-            voteArray.push(vote);
-        }
-        else {
-            voteArray.shift();
-            let vote = parseInt(keyContent);
-            voteArray.push(vote)
-        }
-
-        var vote = keyContent;
-        var dateTime = Date.now();
-        var pubName = key.parentNode.getAttribute("id");
-        var jsonString = JSON.stringify({vote, dateTime, pubName});
-
-        console.log(jsonString)
-
-        $.ajax({
-            type: "POST",
-            url: url,
-            data: jsonString,
-            contentType: 'application/json',
-            success: console.log("Request worked!")
-        })
-        console.log(v++);
+        var voteObject = new Object();
+        voteObject.voteValue = key.value;
+        voteObject.date = Date.now();
+        voteObject.pubName = key.parentNode.getAttribute("id");
+        voteObject.url = '/voted';
+        sendAndReceiveJSON(voteObject, voteObject.url, "post");
     }
 });
+
+var v = 0;
+
+/**
+ * Converts data to JSON before sending it to the url using the method specified.
+ * @param {Object} data
+ * @param {string} url
+ * @param {string} method
+ */
+async function sendAndReceiveJSON(data, url, method) {
+    console.log("The data that is sent " + JSON.stringify(data));
+
+    // Sends POST request to server at url = '/voted'
+    let response = await fetch(url, {
+        method: method.toUpperCase(),
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify(data)
+    });
+
+    // Results is parsed as JSON
+    let result = await response.json();
+
+    // DEBUG RELATED LINES
+    console.log("The result data: " + result)
+    console.log("The result.colorIndex: " + result.colorIndex);
+    console.log("Request number: " + v++);
+
+    // @Todo Fix the hardcoded id value in call to changeQueueColor
+    changeQueueColour("bulten", result.colorIndex);
+
+}
--- server.js
+++ server.js
@@ -1,49 +1,57 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
+// const calcQueue = require('./js/calculateQueue')
 
 const server = express()
 const port = (process.env.PORT || 3000)
 
-server.use(bodyParser.json())
+server.use(express.json())
+server.use(express.urlencoded({extended: false}))
+
+server.listen(port, () => console.log(`Server started on port: ${port}`))
+
+server.get('/', (req, res) => res.sendFile('index.html', {root: __dirname}));
+
+var requestNumber = 0;
+server.use('/voted', (request, response, next) => {
+    let voteValue = getUpdatedVoteValue(request.body.voteValue);
+    response.status(200).json(voteValue);
+    /*
+    // For debugging purposes
+    console.log("A post request for '/voted' has been registered " + requestNumber++);
+    console.log("Requested voteValue: " + request.body.voteValue);
+    console.log(voteValue)
+     */
+    next()
+})
+
+/**
+ * Dummy function, in future this will return correct voteValue for color change.
+ * Currently returns + 1 of the button number clicked.
+ * @param voteValue
+ * @return {*}
+ */
+function getUpdatedVoteValue(voteValue) {
+    var temp = new Object()
+    switch(parseInt(voteValue)) {
+        case 1:
+            temp.colorIndex = parseInt(voteValue) + 1;
+            break;
+        case 2:
+            temp.colorIndex = parseInt(voteValue) + 1;
+            break;
+        case 3:
+            temp.colorIndex = 1;
+            break;
+        default:
+            temp.colorIndex = 404;
+            console.log("ERROR: getUpdatedVoteValue entered default case!")
+    }
+    return temp;
+}
 
 server.use("/js", express.static(__dirname + '/js'))
 server.use("/images", express.static(__dirname + '/images'))
 server.use("/css", express.static(__dirname + '/css'))
 server.use("/fonts", express.static(__dirname + '/fonts'))
 
-server.listen(port, () => console.log(`Server started on port: ${port}`))
-
-server.get('/', (req, res) => res.sendFile('index.html', {root: __dirname}));
-
-server.use('/voted', (request, response, next) => {
-    updateQueueLength(request.body.pubName,request.body.vote);
-    response.end("Vote Registered. Connection closed")
-})
-
-
-
-var pubQueues = [[]];
-function updateQueueLength (pubName, vote){
-
-
-    var sum = 0;
-    var finalValue = 0;
-    var tempQueue = [4];
-    for (i = 0; i < tempQueue.length; i++){
-        sum = sum + tempQueue[i];
-    }
-
-    finalValue = Math.round(sum / tempQueue.length);
-
-    return finalValue;
-
-}
-// server.use is a middleware function that. This means that it process all types of requests.
-// To limit it to a smaller subset, set the first parameter string to be the sub-url you want to process.
-// To no block further middleware calls include the "next" parameter in the anonymous function that is call.
-// server.use("/js", (req, res, next) => {
-//     express.static(__dirname + '/js');
-//     next();
-// });
-
-// Don't forget to close the connection pls :3
