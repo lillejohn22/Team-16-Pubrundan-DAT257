57adad3f2dd487c22b61f3c7508326789935472b
Johan <johan.laberg@outlook.com>
2020-04-29 14:21:45 +0200

    fixing stuff

--- server.js
+++ server.js
@@ -11,23 +11,6 @@ server.get('/', function (req, res) {
     //res.statusCode(200)
 });
 
-// server.use is a middleware function that. This means that it process all types of requests.
-// To limit it to a smaller subset, set the first parameter string to be the sub-url you want to process.
-// To no block further middleware calls include the "next" parameter in the anonymous function that is call.
-// server.use("/js", (req, res, next) => {
-//     express.static(__dirname + '/js');
-//     next();
-// });
-
-// Don't forget to close the connection pls :3
-
-
-
-server.post('/ReceiveJSON', function(req, res){
-    console.log(req.body);
-    res.send("ok");
-});
-
 var s = 0;
 server.use('/voted', (request, response, next) => {
     bodyParser.json();
