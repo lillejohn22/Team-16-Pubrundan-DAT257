99e6b64e81d634151683eeeb3717348e6c5e7d00
ThomasT2 <jintont@student.chalmers.se>
2020-05-06 17:15:28 +0200

    CalculateQueue update
    The ints of 1,2,3 are checked and they represent either short, medium or long queue. The int with most occurences is returned.

--- js/calculateQueue.js
+++ js/calculateQueue.js
@@ -1,42 +1,56 @@
 
 //Calculates the average of the latest queue entries.
 /**
- * Beräknar den nya kön för den pub med queueID. Där newVote är en ny röst och
- * voteTime is when the vote was placed.
+ * Calculates the new queue for the pub with the corresponding queueID. The newVote is
+ * the latest new vote session and voteTime is when the vote was placed and it will be
+ * represented with an interval.
  * @param String queueID, int newVote, int voteTime
  * @returns int newQueueLength for the pub with queueID.
  */
 
-function calculateQueue (queue){
+function calculateQueue(queueID, newVote, voteTime){
 
-    //const queueValue <-- The amount of people in the queue at a given time!
+    // Variables for every different queue option
+    var shortTotal = 0;
+    var mediumTotal = 0;
+    var longTotal = 0;
 
-    var tempQueue = queue;
-    var strLevels = "Short, Medium, Long";
-    var resQueue = "";
-
-    if (tempQueue.length <= 10){
-        return (resQueue = strLevels.slice(0,5));
-    }
-    else if(tempQueue.length >=11 && tempQueue.length<=20){
-        return (resQueue = strLevels.slice(7,13));
-    }
-    else if(tempQueue.length >=30){
-        return (resQueue = strLevels.slice(15,19));
-    }
-
-    // The Old Queue function below!
-    /*
-    var tempQueue = queue;
-    var sum = 0;
-    var finalValue = 0;
-
-    for (i = 0; i < tempQueue.length; i++){
-         sum = sum + tempQueue[i];
+    // 1 = short, 2 = medium, 3 = long
+    for(var i=0;i<newVote.length;i++){
+        if(newVote[i] === 1) {
+            shortTotal++;
+        }
+        else if(newVote[i] === 2){
+            mediumTotal++;
+        }
+        else if(newVote[i] === 3){
+            longTotal++;
+        }
     }
 
-    finalValue = Math.round(sum / tempQueue.length);
+    // Return Pubname and queue type
+    if(shortTotal > (mediumTotal || longTotal)){
+        return queueID + "Short";
+    }
 
-    return finalValue;
-    */
+    if(mediumTotal > (shortTotal || longTotal)){
+        return queueID + "Medium";
+    }
+
+    if(longTotal > (shortTotal || mediumTotal)){
+        return queueID + "Long";
+    }
+
+    if((longTotal === mediumTotal) || (longTotal === shortTotal)){
+        return queueID + "Long"
+    }
+
+    if((mediumTotal === longTotal) || (mediumTotal === shortTotal)){
+        return queueID + "Medium"
+    }
+
+    if((shortTotal === longTotal) || (shortTotal === mediumTotal)){
+        return queueID + "Short"
+    }
+
 }
