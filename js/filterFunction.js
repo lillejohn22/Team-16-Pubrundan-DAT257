ce0ffc8140e29ac04c83271405618487dece29bf
Emma <emmp@student.chalmers.se>
2020-05-14 21:24:14 +0200

    Fix the filter function. Does not work together with the search & sort functions, though.

--- js/filterFunction.js
+++ js/filterFunction.js
@@ -1,90 +1,156 @@
-/**
-* Filters pubs.
- *
- * @author Jennifer Krogh
- * @author Emma Pettersson
-*/
-
-
 $(window).on('load', function() {
     var allPubs = ["wijkanders", "bulten", "japripps", "gasquen", "cafec", "11an", "rodarummet", "ventren", "basen",
-        "goldeni", "kajsabaren", "focus", "hubben21", "fortnox", "gastownospritkoket", "winden", "jarnvagspub", "zaloonen",
-        "pignwhistle"];
+        "goldeni", "kajsabaren", "focus", "hubben21", "fortnox", "gastownospritkoket", "winden", "jarnvagspub",
+        "zaloonen", "pignwhistle"];
 
-    var checkboxes = document.getElementsByClassName("filterButtons");
+    var currentFilters = [];
 
-    // Get the data from pub-data.json
-    $.getJSON('../pub-data.json', function (data) {
-        // Send data to function processJSON to be able to access outside of getJSON call
-        filterFunction(data);
+    // Event listeners for the checkboxes
+    $('.filter-checkbox').click(function (event) {
+        getJSONData($(event.target));
     });
+
+
+    /**
+     * Get the data from pub-data.json
+     *
+     * @param obj
+     */
+    function getJSONData(obj) {
+        $.getJSON('../pub-data.json', function (data) {
+            // Pass the data on to the filter function, along with the object that called the function
+            filterFunction(data, obj);
+        });
+    }
 
 
     /**
      * Filter out the pubs that doesn't match the checked checkboxes.
      *
      * @param data - the data from pub-data.json
+     * @param obj  - the checkbox that was pressed
      */
-    function filterFunction(data) {
-        var filteredList = [];
-        var hiddenList = [];
+    function filterFunction(data, obj) {
+        var filter = $(obj).attr('id').replace("-checkbox", "");
 
-        for (let i = 0; i < checkboxes.length; i++) {
-            if (checkboxes[i].checked) {
-                if (checkboxes[i].id === "open") {
-                    for (let j = 0; j < pubList.length; j++) {
-                        const pubItem = document.getElementById(j);
+        if (currentFilters.includes(filter)) {
+            removeFilter(data, filter);
+        } else {
+            addFilter(data, filter);
+        }
+    }
 
-                        if (pubItem.classList.contains('pub-closed')) {
-                            filteredList.push(j);
-                        }
-                    }
-                } else {
-                    for (let k = 0; k < pubList.length; k++) {
-                        var filterElems = data[k].filter;
 
-                        if (filterElems.includes(checkboxes[i])) {
-                            filteredList.push(k);
-                        }
-                    }
-                }
-            }
+    /**
+     * Adds a new filter to the current filters, and updates the pub list.
+     *
+     * @param data   - the data from pub-data.json
+     * @param filter - the filter to apply
+     */
+    function addFilter(data, filter) {
+        currentFilters.push(filter);
+
+        resetPubs();
+
+        updateFilter(data, filter);
+
+        reorderPubs();
+    }
+
+
+    /**
+     * Removes a filter from the current filters, and updates the pub list.
+     *
+     * @param data   - the data from pub-data.json
+     * @param filter - the filter to apply
+     */
+    function removeFilter(data, filter) {
+        currentFilters = currentFilters.filter(function(e) {
+            return e !== filter
+        });
+
+        resetPubs();
+
+        for (let i = 0; i < currentFilters.length; i++) {
+            updateFilter(data, currentFilters[i]);
         }
 
-        // If no checkboxes have been checked, then all pubs should be displayed.
-        if (filteredList.length === 0) {
-            pubList = allPubs;
-        } else {
-            // Empties the pub list.
-            pubList = [];
+        reorderPubs();
+    }
 
-            // Removes duplicates from the filtered list.
-            $.each(filteredList, function (n, el) {
-                if ($.inArray(el, pubList) === -1) {
-                    pubList.push(el);
+
+    /**
+     * Updates which pubs are visible/invisible.
+     *
+     * @param data   - the data from pub-data.json
+     * @param filter - the filter to apply
+     */
+    function updateFilter(data, filter) {
+        for (let i = 0; i < allPubs.length; i++) {
+            let pub = allPubs[i];
+
+            if (filter === "open") {
+                if (!document.getElementById(pub).classList.contains("pub-closed")) {
+                    document.getElementById(pub).classList.add("visible");
+                    document.getElementById(pub).classList.remove("invisible");
+                } else {
+                    document.getElementById(pub).classList.add("invisible");
+                    document.getElementById(pub).classList.remove("visible");
                 }
-            });
-
-            // Adds the remaining pubs to a hidden list.
-            for (let o = 0; o < allPubs.length; o++) {
-                if (!filteredList.includes(o)) {
-                    hiddenList.push(o);
+            } else {
+                if (data[pub].filter.includes(filter)) {
+                    document.getElementById(pub).classList.add("visible");
+                    document.getElementById(pub).classList.remove("invisible");
+                } else {
+                    document.getElementById(pub).classList.add("invisible");
+                    document.getElementById(pub).classList.remove("visible");
                 }
-            }
-
-            // Removes old order tags and add the new.
-            for (let l = 0; l < pubList.length; l++) {
-                removeOrderTags(l);
-                addOrderTag(l);
-            }
-
-            // Makes remaining pubs hidden.
-            for (let m = 0; m < hiddenList.length; m++) {
-                removeOrderTags(m);
-                document.getElementById(hiddenList[m]).classList.add('.hidden-xs-up');
             }
         }
     }
+
+
+    /**
+     * Fixes the ordering of the grid elements.
+     */
+    function reorderPubs() {
+        for (let i = 0; i < allPubs.length; i++) {
+            let pub = allPubs[i];
+
+            console.log(pub);
+            console.log(document.getElementById(pub).classList.contains("visible"));
+
+            if (document.getElementById(pub).classList.contains("visible")) {
+                document.getElementById(pub).classList.add("order-0");
+                document.getElementById(pub).classList.remove("order-19");
+                removeOrderTags(i);
+            } else {
+                document.getElementById(pub).classList.remove("order-0");
+                document.getElementById(pub).classList.add("order-19");
+                removeOrderTags(i);
+            }
+        }
+    }
+
+
+    /**
+     * Resets the visibility state of all the pubs.
+     */
+    function resetPubs() {
+        for (let i = 0; i < allPubs.length; i++) {
+            document.getElementById(allPubs[i]).classList.remove("invisible");
+            document.getElementById(allPubs[i]).classList.add("visible");
+        }
+    }
+
+
+    /**
+     * Resets the checkboxes and the current filters.
+     * Use when sorting and searching.
+     */
+    function resetCheckboxes() {
+        for (let i = 0; i < currentFilters.length; i++) {
+            document.getElementById(currentFilters[i] + "-checkbox").click();
+        }
+    }
 });
-
-
